8b0a2d631ee7bc05878962552be7cac7308d48fb
aravindanaronraj <aravindanaronraj@gmail.com>
2026-08-08 14:47:12 +0530

    Fix CORS for frontend

--- src/server.js
+++ src/server.js
@@ -22,7 +22,14 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use("/cart", cartRoutes);
 app.use("/orders", orderRoutes);
